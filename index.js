88a0f5fef8585c4312f2c3bed8dce55a046c701c
Harsh <agr.harsh.2000@gmail.com>
2023-04-16 19:53:51 +0530

    make it dynamic by adding javascript

---
+++ index.js
@@ -0,0 +1,339 @@
+
+const movies = [
+   
+
+  ];
+
+  const movieData = {
+    movies: {
+      recommendedMovies: [
+        {
+          name: "Movie 1",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "p/poster 1.png"
+        },
+        {
+          name: "Movie 2",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "p/poster 2.png"
+        },
+        {
+          name: "Movie 3",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "p/poster 3.png"
+        },
+        {
+          name: "Movie 4",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "p/poster 4.png"
+        },
+        {
+          name: "Movie 5",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "p/poster 5.png"
+        },
+        {
+            name: "Movie 6",
+            description: "Lorem ipsum dolor sit amet.",
+            poster: "p/poster 6.png"
+          },
+          {
+            name: "Movie 7",
+            description: "Lorem ipsum dolor sit amet.",
+            poster: "p/poster 7.png"
+          },
+          {
+            name: "Movie 8",
+            description: "Lorem ipsum dolor sit amet.",
+            poster: "p/poster 8.png"
+          },
+          {
+            name: "Movie 9",
+            description: "Lorem ipsum dolor sit amet.",
+            poster: "p/poster 9.png"
+          },
+          {
+            name: "Movie 10",
+            description: "Lorem ipsum dolor sit amet.",
+            poster: "p/poster 10.png"
+          }
+      ],
+      popularMovies: [
+        {
+          name: "Movie Name 1",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc1.jpg"
+        },
+        {
+          name: "Movie Name 2",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc2.jpg"
+        },
+        {
+          name: "Movie Name 3",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc3.jpg"
+        },
+        {
+          name: "Movie Name 4",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc4.jpg"
+        },
+
+        {
+          name: "Movie Name 5",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc5.jpg"
+        },
+
+        {
+          name: "Movie Name 6",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc1.jpg"
+        },
+        {
+          name: "Movie Name 7",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc2.jpg"
+        },
+
+        {
+          name: "Movie Name 8",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc3.jpg"
+        },
+
+        {
+          name: "Movie Name 9",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc4.jpg"
+        },
+
+        {
+          name: "Movie Name 10",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc5.jpg"
+        }
+      ],
+      Shows: [
+        {
+          name: "Movie Name 1",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m1.jpg"
+        },
+        {
+          name: "Movie Name 2",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m2.jpg"
+        },
+        {
+          name: "Movie Name 3",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m3.jpg"
+        },
+        {
+          name: "Movie Name 4",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m4.jpg"
+        },
+
+        {
+          name: "Movie Name 5",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m5.jpg"
+        },
+
+        {
+          name: "Movie Name 6",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m6.jpg"
+        },
+        {
+          name: "Movie Name 7",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m7.jpg"
+        },
+
+        {
+          name: "Movie Name 8",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m8.jpg"
+        },
+
+        {
+          name: "Movie Name 9",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m9.jpg"
+        },
+
+        {
+          name: "Movie Name 10",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "m/m10.jpg"
+        }
+      ],
+      ClassicMovies: [
+        {
+          name: "Movie Name 1",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f1.jpg"
+        },
+        {
+          name: "Movie Name 2",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f2.jpg"
+        },
+        {
+          name: "Movie Name 3",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f3.jpg"
+        },
+        {
+          name: "Movie Name 4",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f4.jpg"
+        },
+
+        {
+          name: "Movie Name 5",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f5.jpg"
+        },
+
+        {
+          name: "Movie Name 6",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f6.jpg"
+        },
+        {
+          name: "Movie Name 7",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f7.jpg"
+        },
+
+        {
+          name: "Movie Name 8",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f8.jpg"
+        },
+
+        {
+          name: "Movie Name 9",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f9.jpg"
+        },
+
+        {
+          name: "Movie Name 10",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "f/f1.jpg"
+        }
+      ],
+      Movies: [
+        {
+          name: "Movie Name 1",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc1.jpg"
+        },
+        {
+          name: "Movie Name 2",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc2.jpg"
+        },
+        {
+          name: "Movie Name 3",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc3.jpg"
+        },
+        {
+          name: "Movie Name 4",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc4.jpg"
+        },
+
+        {
+          name: "Movie Name 5",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc5.jpg"
+        },
+
+        {
+          name: "Movie Name 6",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc1.jpg"
+        },
+        {
+          name: "Movie Name 7",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc2.jpg"
+        },
+
+        {
+          name: "Movie Name 8",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc3.jpg"
+        },
+
+        {
+          name: "Movie Name 9",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc4.jpg"
+        },
+
+        {
+          name: "Movie Name 10",
+          description: "Lorem ipsum dolor sit amet.",
+          poster: "rc/rc5.jpg"
+        }
+      ]
+    },
+    order: ["popularMovies", "recommendedMovies","Shows","ClassicMovies","Movies"]
+  };
+  
+function generateslides(titles){
+  for(let i=0;i<movieData.order.length;i++)
+  {
+  console.log(movieData.movies[movieData.order[i]])
+  const allslides=document.querySelector(".allslides");
+  const titleElement = document.createElement("h1");
+  titleElement.classList.add("title");
+  titleElement.textContent = movieData.order[i];
+  allslides.appendChild(titleElement);
+  
+  const container=document.querySelector(".card-container");
+  movieData.movies[movieData.order[i]].forEach((movie)=>{
+  const card=document.createElement("div");
+  card.classList.add("card");
+  const img = document.createElement("img");
+  img.classList.add("card-img");
+  img.src=movie.poster;
+  img.alt ="";
+  const body=document.createElement("div");
+  body.classList.add("card-body");
+  const title=document.createElement("h2");
+  title.classList.add("name");
+  title.textContent=movie.name;
+  const description=document.createElement("h6");
+  description.classList.add("des");
+  description.textContent=movie.description;
+  const button = document.createElement("button");
+  button.classList.add("watchlist-btn");
+  button.textContent = "add to watchlist";
+    
+    body.appendChild(title);
+    body.appendChild(description);
+    body.appendChild(button);
+    card.appendChild(img);
+    card.appendChild(body);
+    container.appendChild(card);
+    allslides.appendChild(container);
+  })};
+  
+}
+
+
+const showsContainer1 = generateslides();
+
+
+
+
